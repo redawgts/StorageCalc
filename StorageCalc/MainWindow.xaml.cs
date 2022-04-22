767cb04a68d270dd81a66c99cc5aa704b6037b39
Joshua Camara <redawgts@gmail.com>
2022-04-22 02:17:19 -0400

    Moved main logic into it's own class

--- StorageCalc/MainWindow.xaml.cs
+++ StorageCalc/MainWindow.xaml.cs
@@ -1,9 +1,10 @@
 ﻿
 namespace StorageCalc
 {
+    using StorageCalc.ViewModels;
     using System;
     using System.Windows;
-    
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -16,95 +17,15 @@ namespace StorageCalc
 
         private void BtnCalculate_OnClick(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                var diskCount = Convert.ToInt32(this.TxtDiskCount.Text);
-                var diskSpace = Convert.ToDouble(this.TxtDiskSpace.Text);
+            var vm = new MainViewModel();
 
-                var diskspaceInBytes = diskSpace * 1000000000000L;
-                var divisor = 1024L * 1024L * 1024L * 1024L;
-                var realDiskSpace = (double)diskspaceInBytes / (double)divisor;
+            (string txtTotalSize, string txtFaultTolerance) = vm.Calculate(
+                TxtDiskCount.Text, TxtDiskSpace.Text, RbnRaid0.IsChecked,
+                RbnRaid1.IsChecked, RbnRaid5.IsChecked,
+                RbnRaid6.IsChecked, RbnRaid10.IsChecked);
 
-                var usableSpace = 0.0d;
-                var faultTolerance = string.Empty;
-
-                if (this.RbnRaid0.IsChecked == true)
-                {
-                    usableSpace = diskCount * realDiskSpace;
-                    faultTolerance = "keine";
-                }
-
-                if (this.RbnRaid1.IsChecked == true)
-                {
-                    if (diskCount == 2)
-                    {
-                        usableSpace = realDiskSpace;
-                        faultTolerance = "1 Platte";
-                    }
-                    else
-                    {
-                        MessageBox.Show("Genau 2 Platten benötigt");
-                        return;
-                    }
-                }
-
-                if (this.RbnRaid5.IsChecked == true)
-                {
-                    if (diskCount >= 3)
-                    {
-                        usableSpace = (diskCount - 1) * realDiskSpace;
-                        faultTolerance = "1 Platte";
-                    }
-                    else
-                    {
-                        MessageBox.Show("Min. 3 Platten notwendig");
-                        return;
-                    }
-                }
-
-                if (this.RbnRaid6.IsChecked == true)
-                {
-                    if (diskCount >= 4)
-                    {
-                        usableSpace = (diskCount - 2) * realDiskSpace;
-                        faultTolerance = "2 Platten";
-                    }
-                    else
-                    {
-                        MessageBox.Show("Min. 4 Platten notwendig");
-                        return;
-                    }
-                }
-
-                if (this.RbnRaid10.IsChecked == true)
-                {
-                    if (diskCount % 2 == 0 && diskCount >= 4)
-                    {
-                        usableSpace = (diskCount - 2) * realDiskSpace;
-                        faultTolerance = "Min. 1 Platte";
-                    }
-                    else
-                    {
-                        MessageBox.Show("Min. 4 Platten und gerade Anzahl notwendig");
-                        return;
-                    }
-                }
-
-                this.TxtTotalSize.Text = Math.Round(usableSpace, 2) + " TB";
-                this.TxtFaultTolerance.Text = faultTolerance;
-            }
-            catch (FormatException)
-            {
-                MessageBox.Show("Eingabe prüfen, bitte nur Zahlen eingeben");
-            }
-            catch (OverflowException)
-            {
-                MessageBox.Show("Overflow, bitte kleinere Zahlen eingeben");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Da ging was schief:\r\n\r\n" + ex);
-            }
+            TxtTotalSize.Text = txtTotalSize;
+            TxtFaultTolerance.Text = txtFaultTolerance;
         }
     }
 }
---
+++ StorageCalc/ViewModels/MainViewModel.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace StorageCalc.ViewModels
+{
+    public class MainViewModel
+    {
+        public (string txtTotalSize, string txtFaultTolerance) Calculate(
+            string txtDiskCount, string txtDiskSpace,
+            bool? raid0, bool? raid1, bool? raid5, bool? raid6, bool? raid10)
+        {
+            try
+            {
+                var diskCount = Convert.ToInt32(txtDiskCount);
+                var diskSpace = Convert.ToDouble(txtDiskSpace);
+
+                var diskspaceInBytes = diskSpace * 1000000000000L;
+                var divisor = 1024L * 1024L * 1024L * 1024L;
+                var realDiskSpace = (double)diskspaceInBytes / (double)divisor;
+
+                var usableSpace = 0.0d;
+                var faultTolerance = string.Empty;
+
+                if (raid0 == true)
+                {
+                    usableSpace = diskCount * realDiskSpace;
+                    faultTolerance = "keine";
+                }
+
+                if (raid1 == true)
+                {
+                    if (diskCount == 2)
+                    {
+                        usableSpace = realDiskSpace;
+                        faultTolerance = "1 Platte";
+                    }
+                    else
+                    {
+                        MessageBox.Show("Genau 2 Platten benötigt");
+                        return default;
+                    }
+                }
+
+                if (raid5 == true)
+                {
+                    if (diskCount >= 3)
+                    {
+                        usableSpace = (diskCount - 1) * realDiskSpace;
+                        faultTolerance = "1 Platte";
+                    }
+                    else
+                    {
+                        MessageBox.Show("Min. 3 Platten notwendig");
+                        return default;
+                    }
+                }
+
+                if (raid6 == true)
+                {
+                    if (diskCount >= 4)
+                    {
+                        usableSpace = (diskCount - 2) * realDiskSpace;
+                        faultTolerance = "2 Platten";
+                    }
+                    else
+                    {
+                        MessageBox.Show("Min. 4 Platten notwendig");
+                        return default;
+                    }
+                }
+
+                if (raid10 == true)
+                {
+                    if (diskCount % 2 == 0 && diskCount >= 4)
+                    {
+                        usableSpace = (diskCount - 2) * realDiskSpace;
+                        faultTolerance = "Min. 1 Platte";
+                    }
+                    else
+                    {
+                        MessageBox.Show("Min. 4 Platten und gerade Anzahl notwendig");
+                        return default;
+                    }
+                }
+
+                return (Math.Round(usableSpace, 2) + " TB", faultTolerance);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Eingabe prüfen, bitte nur Zahlen eingeben");
+                return default;
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("Overflow, bitte kleinere Zahlen eingeben");
+                return default;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Da ging was schief:\r\n\r\n" + ex);
+                return default;
+            }
+        }
+    }
+}
